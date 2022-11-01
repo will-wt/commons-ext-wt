5f3efaea2578556ee16c6a6100051bebdb578c42
Will.WT <willwt@qq.com>
2022-11-01 14:06:47 +0800

    GsonHelper int/long to long

--- src/main/java/com/will/commons/json/GsonHelper.java
+++ src/main/java/com/will/commons/json/GsonHelper.java
@@ -2,18 +2,9 @@ package com.will.commons.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.internal.LinkedTreeMap;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.ToNumberPolicy;
 
-import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Will.WT
@@ -22,12 +13,19 @@ import java.util.Map;
 public class GsonHelper {
 
     private static Gson gson = new GsonBuilder()
-            .registerTypeAdapter(new TypeToken<Map<String, Object>>(){}.getType(), new MapTypeAdapter())
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            // 反序列化时object转number策略，int/long转成long
+            .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
             .create();
 
 
-    public static String toJson(Object o){
-        return getInstance().toJson(o);
+
+    public static String toJson(Object src){
+        return getInstance().toJson(src);
+    }
+
+    public static String toJson(Object src, Type type){
+        return getInstance().toJson(src, type);
     }
 
     public static <T> T fromJson(String json, Class<T> classz){
@@ -40,73 +38,6 @@ public class GsonHelper {
 
     public static Gson getInstance(){
         return gson;
-    }
-
-
-
-    /**
-     * Map反序列化
-     */
-    public static class MapTypeAdapter extends TypeAdapter<Object> {
-
-        @Override
-        public Object read(JsonReader in) throws IOException {
-            JsonToken token = in.peek();
-            switch (token) {
-                case BEGIN_ARRAY:
-                    List<Object> list = new ArrayList<Object>();
-                    in.beginArray();
-                    while (in.hasNext()) {
-                        list.add(read(in));
-                    }
-                    in.endArray();
-                    return list;
-
-                case BEGIN_OBJECT:
-                    Map<String, Object> map = new LinkedTreeMap<String, Object>();
-                    in.beginObject();
-                    while (in.hasNext()) {
-                        map.put(in.nextName(), read(in));
-                    }
-                    in.endObject();
-                    return map;
-
-                case STRING:
-                    return in.nextString();
-
-                case NUMBER:
-                    // 改写数字的处理逻辑，将数字值分为整型与浮点型。
-                    double dbNum = in.nextDouble();
-
-                    // 数字超过long的最大值，返回浮点类型
-                    if (dbNum > Long.MAX_VALUE) {
-                        return dbNum;
-                    }
-
-                    // 判断数字是否为整数值
-                    long lngNum = (long) dbNum;
-                    if (dbNum == lngNum) {
-                        return lngNum;
-                    } else {
-                        return dbNum;
-                    }
-
-                case BOOLEAN:
-                    return in.nextBoolean();
-
-                case NULL:
-                    in.nextNull();
-                    return null;
-
-                default:
-                    throw new IllegalStateException();
-            }
-        }
-
-        @Override
-        public void write(JsonWriter out, Object value) throws IOException {
-            // 序列化无需实现
-        }
     }
 
 }
--- src/test/java/com/will/commons/json/GsonHelperTest.java
+++ src/test/java/com/will/commons/json/GsonHelperTest.java
@@ -1,15 +1,19 @@
 package com.will.commons.json;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.ParseException;
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Will.WT
@@ -20,48 +24,121 @@ public class GsonHelperTest {
     private Map<String, Object> testMap;
     private Person person;
     private Gson gson;
+    private String dateStr = "2022-10-29 12:20:30";
 
     @Before
-    public void init(){
+    public void init() throws ParseException {
+        Date date = DateUtils.parseDate(dateStr, "yyyy-MM-dd HH:mm:ss");
+
         testMap = new HashMap<String, Object>();
         testMap.put("int", 100);
         testMap.put("long", 91234567890L);
         testMap.put("double", 99.9);
         testMap.put("str", "hello");
-        testMap.put("date", new Date());
+        testMap.put("date", date);
 
         Map<String, Object> subMap = new HashMap<String, Object>();
         subMap.put("int", 100);
         subMap.put("long", 91234567890L);
         subMap.put("double", 99.9);
         subMap.put("str", "hello");
-        subMap.put("date", new Date());
+        subMap.put("date", date);
         testMap.put("subMap", subMap);
 
-        Person.builder()
-                .gmtCreate(new Date())
+
+
+        Map<String, Object> scoreMap = new HashMap<String, Object>();
+        scoreMap.put("yuwen", 90);
+        scoreMap.put("shuxue", 100);
+        scoreMap.put("yingyu", 85);
+
+        person = Person.builder()
+                .gmtCreate(date)
                 .userId(100001L)
                 .name("ZhangSan")
                 .age(40)
                 .addressList(Arrays.asList(new String[]{"hangzhou", "shanghai", "beijing"}))
-//                .scoreMap(new HashMap<String, Object>(){"yuwen":80})
+                .scoreMap(scoreMap)
+                .children(Lists.newArrayList(Person.builder()
+                                .gmtCreate(date)
+                                .userId(100002L)
+                                .name("ZhangSi")
+                                .age(16)
+                                .addressList(Arrays.asList(new String[]{"hangzhou", "shanghai", "beijing"}))
+                                .scoreMap(scoreMap)
+                                .build())
+                )
               .build();
-
 
         gson = new GsonBuilder().create();
     }
 
     @Test
-    public void testToJson(){
-        String json = gson.toJson(testMap);
-        System.out.println("toJson="+ json);
+    public void testJsonByMap(){
+        String json1 = gson.toJson(testMap);
+        System.out.println("json1="+ json1);
+        assertFalse(json1.indexOf("100.0") > 0);
 
 
-        Map<String, Object> map = GsonHelper.fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+        Map<String, Object> map = gson.fromJson(json1, new TypeToken<Map<String, Object>>(){}.getType());
+        assertNotNull(map);
 
-        System.out.println(map.get("int"));
+        System.out.println("json2="+ gson.toJson(map));
+
+        assertEquals( 100.0, map.get("int"));
+        assertEquals(100.0, ((Map)map.get("subMap")).get("int"));
     }
 
+    @Test
+    public void testJsonByBean(){
+        String json1 = gson.toJson(person);
+        System.out.println("json1="+ json1);
+        assertFalse(json1.indexOf("85.0") > 0);
+
+        Person person = gson.fromJson(json1, Person.class);
+        assertNotNull(person);
+        assertNotNull(person.getUserId());
+        System.out.println("json2="+ gson.toJson(person));
+    }
+
+    @Test
+    public void testGsonHelper(){
+        String json1 = GsonHelper.toJson(testMap);
+        System.out.println("json1="+ json1);
+        assertFalse(json1.indexOf("100.0") > 0);
+
+        String json2 = GsonHelper.toJson(person);
+        System.out.println("json2="+ json2);
+        assertFalse(json1.indexOf("85.0") > 0);
+
+        Map<String, Object> map = GsonHelper.fromJson(json1, new TypeToken<Map<String, Object>>() {}.getType());
+        assertNotNull(map);
+        System.out.println("json1-1="+ GsonHelper.toJson(map));
+        assertEquals( 100L, map.get("int"));
+        assertEquals(100L, ((Map)map.get("subMap")).get("int"));
+
+        Person personTemp = GsonHelper.fromJson(json2, Person.class);
+        assertNotNull(personTemp);
+        assertNotNull(personTemp.getUserId());
+        System.out.println("json2-2="+ GsonHelper.toJson(personTemp));
+    }
+
+    @Test
+    public void testListMap(){
+        List<Map<String, Object>> testList = new ArrayList<Map<String, Object>>();
+        testList.add(testMap);
+
+        String json1 = GsonHelper.toJson(testList);
+        assertNotNull(json1);
+        System.out.println("json1="+ json1);
+
+        List<Map<String, Object>> listTemp = GsonHelper.fromJson(json1, new TypeToken<List<Map<String, Object>>>() {}.getType());
+        assertNotNull(listTemp);
+        System.out.println("json2="+ GsonHelper.toJson(listTemp));
+
+        assertEquals(100L, listTemp.get(0).get("int"));
+        assertEquals(100L, ((Map)listTemp.get(0).get("subMap")).get("int"));
+    }
 
 
 }
